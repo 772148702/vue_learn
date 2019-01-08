f877d6231f8b907e02fc92c1d12f23056099acd7
monster <77248702@qq.com>
2019-01-08 17:07:16 +0800

    "第一天,页面的初步的排版，但是图片的引用"

--- bootstrap/cnm/src/components/Footer.vue
+++ bootstrap/cnm/src/components/Footer.vue
@@ -1,5 +1,13 @@
 <template>
-    
+  <div>
+    <nav class="navbar navbar-default navbar-fixed-bottom">
+      <div class="container">
+       <h3>云游戏平台</h3>
+      </div>
+    </nav>
+  </div>
+
+
 </template>
 
 <script>
--- bootstrap/cnm/src/main.js
+++ bootstrap/cnm/src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+
+
+
+
 import $ from 'jquery'
 Vue.config.productionTip = false
 require('bootstrap/dist/css/bootstrap.min.css');
